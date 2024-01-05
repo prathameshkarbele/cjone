3aefe535ef30d45198ce271bad48734bea9d4e3e
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2024-01-05 21:23:42 +0530

    p1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./componants/Header/Navbar.css";
+import Navbar from "./componants/Header/Navbar";
+import NewNav from "./componants/newNavBar/NewNav";
+import Maincom from "./componants/home/Maincom";
+import Footer from "./componants/footer/Footer";
+import SignIn from "./componants/signup_signin/SignIn";
+import { SignUp } from "./componants/signup_signin/SignUp";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./componants/cart/Cart";
+import Buynow from "./componants/buynow/Buynow";
+import CircularProgress from '@mui/material/CircularProgress';
+import { useEffect, useState } from "react";
+
+// "proxy": "http://localhost:4001/",
 
 function App() {
+
+  const [data,setData] = useState(false);
+   
+  useEffect(()=>{
+      setTimeout(()=>{
+         setData(true)
+      },2000)
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    {
+      data ? (
+        <>
+           
+      <Navbar />
+      <NewNav />
+      <Routes>
+        <Route path="/" element={<Maincom />}></Route>
+        <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/register" element={<SignUp />}></Route>
+        <Route path="/getproductsone/:id" element={<Cart />}></Route>
+        <Route path="/buynow" element={<Buynow />}></Route>
+      </Routes>
+      <Footer />
+        </>
+      ):(
+        <div className="circle">
+        <CircularProgress />
+          <h2>Loading.....</h2>
+        </div>
+      )
+    }
+      
+    </>
   );
 }
 
